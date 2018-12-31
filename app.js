1c5572a78021fd2669c5af5999e9f54273c98709
Frakcool <frakcool1993@gmail.com>
2018-12-31 17:16:04 -0500

    Added a sides object and changed logic accordingly

--- app.js
+++ app.js
@@ -1,63 +1,63 @@
 //Inspired on Roll Dice by Jacob Schaefer - https://codepen.io/Pyremell/pen/eZGGXX/
 
-//Get each point from dice
-const firstPoint = document.getElementById('first');
-const thirdPoint = document.getElementById('third');
-const fourthPoint = document.getElementById('fourth');
-const fifthPoint = document.getElementById('fifth');
-const sixthPoint = document.getElementById('sixth');
-const seventhPoint = document.getElementById('seventh');
-const ninthPoint = document.getElementById('ninth');
+let pointsMap = new Map()
+pointsMap.set(1, 'first')
+pointsMap.set(3, 'third')
+pointsMap.set(4, 'fourth')
+pointsMap.set(5, 'fifth')
+pointsMap.set(6, 'sixth')
+pointsMap.set(7, 'seventh')
+pointsMap.set(9, 'ninth')
+
 const points = document.getElementsByClassName('point');
 
 const button = document.getElementById('button');
 
 //Our dice object
 let dice = {
-	sides: 6,
-	getRandom () {
-		let number = Math.floor(Math.random() * this.sides) + 1;
+	numberOfSides: 6,
+	sides: [{
+		//Empty object to prevent "undefined", not sure why it happens only for the first element in array.
+	}, {
+		id: 1,
+		points: [5]
+	}, {
+		id: 2,
+		points: [3, 7]
+	}, {
+		id: 3,
+		points: [3, 5, 7]
+	}, {
+		id: 4,
+		points: [1, 3, 7, 9]
+	}, {
+		id: 5,
+		points: [1, 3, 5, 7, 9]
+	}, {
+		id: 6,
+		points: [1, 3, 4, 6, 7, 9]
+	}],
+	getRandom() {
+		let number = Math.floor(Math.random() * this.numberOfSides) + 1;
     	return number;
 	},
-	showPoint (point) {
-		point.style.visibility = 'visible';
+	showPoints(side) {
+		side.points.forEach(function(point) {
+			let pointToDraw = document.getElementById(pointsMap.get(point))
+			pointToDraw.style.visibility = 'visible'
+		})
 	},
-	printNumber (random) {
-		if (random == 1) { 
-			this.showPoint(fifthPoint);
-
-		} else if (random == 2) {
-			this.showPoint(thirdPoint);
-			this.showPoint(seventhPoint);
-
-		} else if (random == 3) {
-			this.showPoint(thirdPoint);
-			this.showPoint(fifthPoint);
-			this.showPoint(seventhPoint);
-
-		} else if (random == 4) {
-			this.showPoint(firstPoint);
-			this.showPoint(thirdPoint);
-			this.showPoint(seventhPoint);
-			this.showPoint(ninthPoint);
-
-		} else if (random == 5) {
-			this.showPoint(firstPoint);
-			this.showPoint(thirdPoint);
-			this.showPoint(fifthPoint);
-			this.showPoint(seventhPoint);
-			this.showPoint(ninthPoint);
-
-		} else if (random == 6) {
-			this.showPoint(firstPoint);
-			this.showPoint(thirdPoint);
-			this.showPoint(fourthPoint);
-			this.showPoint(sixthPoint);
-			this.showPoint(seventhPoint);
-			this.showPoint(ninthPoint);
+	printNumber(random) {
+		console.log(random)
+		for (let i = 1; i <= this.sides.length; i++) {
+			let id = this.sides[i].id
+			if (random == id) {
+				this.showPoints(this.sides[i])
+				break;
+			}
 		}
 	},
-	clear () {
+	clear() {
 		for (let i = 0; i < points.length; i++) {
 			points[i].style.visibility = 'hidden';
 		}
